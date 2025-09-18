5f9dd608bbdc6f4bc0eae573e70e38b547e5534b
melissavargasramirez021-ctrl <melissavargasramirez021@gmail.com>
2025-09-18 09:05:06 -0600

    Create Dem.py
    Ejercicio en cem

---
+++ Dem.py
@@ -0,0 +1,9 @@
+# este es un codigo que revisa si la edad es valida 
+
+edad 
+valida = false 
+while not valida: 
+  edad = int(input("cuantos años tienes? "))
+  if edad <100 and edad > 1 :
+print("perfecto!")
+valida = true
