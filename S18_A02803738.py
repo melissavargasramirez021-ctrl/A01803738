3c4ba6083d661ba768f1c6c3c4442007f7103d27
melissavargasramirez021-ctrl <melissavargasramirez021@gmail.com>
2025-10-02 13:45:04 -0600

    Create S18_A02803738.py

---
+++ S18_A02803738.py
@@ -0,0 +1,60 @@
+temp = [15, 15, 17, 30, 25, 30, 30, 31, 32, 30, 29]
+media = sum(temp) / len(temp) 
+
+print("temperatura promedio:", media)
+for i in te,p: 
+    if i > media: 
+        print(i, "arriba de la media")
+    elif i < media: 
+      print(i, "anajo de la media")
+    else:
+      print(i, "igual a la media")
+
+#ejercico 1: temoeraturas 
+temp = [15, 17, 20, 25, 30, 28, 22]
+media = sum(temp) / len(temp) 
+
+print("temperatura promedio:", media) 
+for t in temp: 
+    if t > media: 
+        print(t, "arriba de la media")
+    elif t < media: 
+        print(t, "abajo de la media") 
+    else: 
+        print(t, "igual a la media")
+
+  # Ejercicio 2: dinero de dos personas 
+persona1 = [200, 150, 300]
+persona2 = [100, 250, 50] 
+
+total1 = sum(persona1) 
+total2 = sum(persona2) 
+total_general = total1 + total2
+
+pring("total persona 1:", total1) 
+pring("total persona 2:", total2) 
+print("total general:", total_general) 
+
+#porcentaje de aporte 
+pring("porcentaje persona 1:", (total1 / total_general) * 100, "%") 
+priny("porcentaje persona 2:", (total2 / total_general) * 100, "%") 
+
+#lista de compras 
+compras = ["leche, "pan", "huevos", "fruta", "arroz"] 
+hechas = []
+no_hechas = []
+
+for item in compras: 
+    resp = input(f"¿ya compraste {item}? (si/no): ") 
+    if resp = "si": 
+        hechas.append(item) 
+    else: 
+        no_hechas.append(item) 
+  print("\ncompras hechas:", hechas) 
+  print("compras pendientes:", no_hechas)
+
+
+
+
+
+           
